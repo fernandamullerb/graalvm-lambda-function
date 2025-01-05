afca3dbdb0af57d432cfb2bb666aac4a04d9cd3a
Fernanda <fernandamullerb@gmail.com>
2025-01-05 15:38:22 -0300

    Initial commit

---
+++ src/main/java/br/com/demo/AppRequestHandler.java
@@ -0,0 +1,18 @@
+package br.com.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+
+public class AppRequestHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+
+    @Override
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent apiGatewayV2HTTPEvent, Context context) {
+        System.out.println("Inside AppRequestHandler::handleRequest()");
+        return APIGatewayV2HTTPResponse.builder()
+                .withStatusCode(200)
+                .build();
+    }
+
+}
